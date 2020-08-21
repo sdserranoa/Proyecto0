ea5925897d9cbbeb15171352efb919b2c1e090fb
Santiago Serrano <sd.serrano@uniandes.edu.co>
2020-08-20 21:29:04 -0500

    final

---
+++ front/build/precache-manifest.f05d0b7be5206cb172faf951c9078f1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "383ffd545d3570f7b41d131110d34bcc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14c73e3c104acf6f6c3c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5df9574e72bd9f248a66",
+    "url": "/static/js/2.aecdfd76.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.aecdfd76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14c73e3c104acf6f6c3c",
+    "url": "/static/js/main.d8b1f776.chunk.js"
+  },
+  {
+    "revision": "23f7e4d2c8397ce55bf6",
+    "url": "/static/js/runtime-main.5fe610d3.js"
+  }
+]);
